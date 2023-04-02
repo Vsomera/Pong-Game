9ba1127b2aa70cb823d17e9d4b74262718849f5e
Vilmor Somera <vsomera77@outlook.com>
2023-04-02 16:14:54 -0700

    updated comments

--- game.py
+++ game.py
@@ -8,16 +8,17 @@ clock = pygame.time.Clock()
 # Main Window Setup
 screen_width = 1200
 screen_height = 960
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Pong')
+screen = pygame.display.set_mode((screen_width, screen_height)) # returns display surface object
+pygame.display.set_caption('Pong')      # gives window a title
 
 while True:
-    # Closes game upon user input
+    # Handles all user inputs
     for event in pygame.event.get():
+        # Closes game upon user input (clicks X on top of screen)
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
     # updates window
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(60) # limits how fast the loop runs (60fps)
