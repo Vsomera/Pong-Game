18c4d002a415859af081519bd6a2bf9cf3c5b3e8
Vincent Wong <vincentwong5609@gmail.com>
2023-04-02 21:37:15 -0700

    animations, controls and opponent bot

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 
 # initiate pygame
 pygame.init()
@@ -33,18 +33,14 @@ grey = pygame.Color('grey12')
 light_grey = (200, 200, 200)
 
 ### Establishes the speed of the ball ###
-ball_speed_x = 5
-ball_speed_y = 5
+ball_speed_x = 7 * random.choice((1, -1))
+ball_speed_y = 7 * random.choice((1, -1))
+ball_speed_y = 7
+player_speed = 0
+opponent_speed = 7
 
-while True:
-    # Handles all user inputs
-    for event in pygame.event.get():
-        # Closes game upon user input (clicks X on top of screen)
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-
+def ball_animation():
+    global ball_speed_x, ball_speed_y
     # Animations
     ball.x += ball_speed_x
     ball.y += ball_speed_y
@@ -53,11 +49,55 @@ while True:
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
+        ball_restart()
     
     # Ball collisions on rectangles
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
+
+def player_animation():
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
+def opponent_animation():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
+while True:
+    # Handles all user inputs
+    for event in pygame.event.get():
+        # Closes game upon user input (clicks X on top of screen)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed +=7
+            if event.key == pygame.K_UP:
+                player_speed -=7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -=7
+            if event.key == pygame.K_UP:
+                player_speed += 7
+
+    ball_animation()
+    player_animation()
+    opponent_animation()
 
     # Visuals -> Colors in the rectangles and screen
     screen.fill(grey) # The entire screen background
